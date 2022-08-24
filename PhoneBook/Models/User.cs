d12b97e50f7f2b3bccda2271bac23106dbe3fc5e
berkayonat <berkayonatdemir@gmail.com>
2022-08-25 02:22:28 +0300

    Add project files.

---
+++ PhoneBook/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email{ get; set; }
+        public string? Address { get; set; }
+        public bool IsJson { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+               
+    }
+}
